62c49621c6b1678078b7d0970445fb3bfaaf484f
jakep <69126561+jakepscripts@users.noreply.github.com>
2022-07-26 22:35:00 -0700

    Update afk.lua

--- afk.lua
+++ afk.lua
@@ -20,7 +20,6 @@ local Home = Window:MakeTab({Name = "Notice", Icon = "rbxassetid://7072978559", 
 Home:AddParagraph("Notice","Click anti-afk to prevent yourself from going idle ingame.")
 
 Home:AddButton({Name = "Anti-Afk!",Callback = function()
-        OrionLib:Destroy()
        notification("Void", "Anti-AFK has now been enabled.", "rbxassetid://10198709880", 3)
         game:service "Players".LocalPlayer.Idled:connect(
                 function()
